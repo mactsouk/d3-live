d6afdec5098630fac85e107de341d617973ed075
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-04-08 14:45:42 +0300

    After talking with Dimitris

--- regular-user.md
+++ regular-user.md
@@ -39,16 +39,84 @@ your data from Lenses.
 
 ### How to create the JavaScript project
 
+In order to create the JavaScript project, you will need to follow the next
+steps:
+
+- Execute `git clone https://github.com/wbkd/webpack-starter.git` inside the directory of your project.
+
+You will then need to make changes to the following files:
+
+- `package.json`
+- `./src/index.html`
+- `./src/scripts/index.js`
+
+The contents of `package.json` should be the following:
+
+	{
+	  "name": "d3-live",
+	  "version": "0.0.1",
+	  "repository": "https://github.com/mactsouk/d3-live.git",
+	  "description": "A D3 Representation of an SQL Processor when Authenticated and using the Lenses API",
+	  "scripts": {
+	    "build": "cross-env NODE_ENV=production webpack --config webpack/webpack.config.prod.js --colors",
+	    "start": "webpack-dev-server --open --config webpack/webpack.config.dev.js"
+	  },
+	  "author": "Lenses.io",
+	  "license": "MIT",
+	  "devDependencies": {
+	    "@babel/core": "^7.9.0",
+	    "@babel/plugin-proposal-class-properties": "^7.8.3",
+	    "@babel/plugin-syntax-dynamic-import": "^7.8.3",
+	    "@babel/preset-env": "^7.9.0",
+	    "babel-loader": "^8.1.0",
+	    "clean-webpack-plugin": "^3.0.0",
+	    "copy-webpack-plugin": "^5.1.1",
+	    "cross-env": "^7.0.2",
+	    "eslint": "^6.8.0",
+	    "eslint-loader": "^3.0.3",
+	    "file-loader": "^5.1.0",
+	    "html-webpack-plugin": "^4.0.0-beta.11",
+	    "mini-css-extract-plugin": "^0.9.0",
+	    "webpack": "^4.42.1",
+	    "webpack-cli": "^3.3.11",
+	    "webpack-dev-server": "^3.10.3",
+	    "webpack-merge": "^4.2.2"
+	  },
+	  "dependencies": {
+	    "@babel/polyfill": "^7.8.7",
+	    "axios": "^0.19.2",
+	    "core-js": "^3.6.4",
+	    "rxjs": "^6.5.5",
+	    "ws": "^7.2.3"
+	  }
+	}
+
+Now you should execute
+
+- Execute `npm install` if you are using `npm`.
+- Execute `yarn` if you are using `yarn`.
+
+If you decide to use our GitHub repository, you will need to execute the following
+command only:
+
+- `git clone git@github.com:mactsouk/d3-live.git`
+
 
 ### How to login
 
 The following JavaScript code is used for logging in Lenses in order to get the data.
 
 
+In order to login you are using Lenses **REST API**.
+
+
 ### How to get data from Lenses
 
 The following JavaScript code is used for getting the data from Lenses.
 
+
+In order to get data from Lenses, you will need to create a **WebSocket**
+connection.
 
 
 ### The data format
@@ -73,11 +141,22 @@ Note that a visualization tells you information about the data you put to it.
 Therefore if you do not like the generated output, you should rethink the
 value of your data as well as the visualization type you used.
 
+
 ### The final output
 
 In this section we are going to see the generated visualization. Note that each
 time you load the HTML file, you will get a different output as we are working
 with dynamic data, that is data that changes over time.
+
+
+## Presenting the files
+
+
+### The `./src/index.html` file
+
+
+### The `./src/scripts/index.js` file
+
 
 
 ## Next steps
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,47 +1,77 @@
 import axios from 'axios';
 import Websocket from 'ws';
-
+import { of } from 'rxjs';
+import { tap, filter, toArray, map, reduce, merge } from 'rxjs/operators';
 const username = 'admin';
 const password = 'admin';
-
 const authenticationUrl = 'http://localhost:4700/api/login'; // You need to change that PORT to your local machine PORT that Lenses is running on.
-const topicDataUrl = 'ws://localhost:4700/api/ws/v2/sql/scroll';
-
+const topicDataUrl = 'ws://localhost:4700/api/ws/v2/sql/execute';
 const authenticationRequest = () => axios.post(authenticationUrl, {
   user: username,
   password
 })
-  .then((response) => { console.log('USER AUTHENTICATED'); return response.data; })
+  .then((response) => response.data)
   .catch((error) => console.error(console.error('Error Response', error)));
-
-
 const wsRequest = () => authenticationRequest().then(reqToken => {
   const webSocketRequest = new WebSocket(topicDataUrl);
-
-
   const firstMessage = {
     token: reqToken,
-    limit: 200,
-    partition: 0,
-    start: 0,
-    table: "cc_payments",
-    type: "offset"
+    stats: 2,
+    sql: "SELECT * FROM cc_payments LIMIT 200",
+    live: false
   };
-
+  const messages = [];
   webSocketRequest.onopen = () => {
-    console.log('Websocket Endpoint OPEN');
     webSocketRequest.send(JSON.stringify(firstMessage));
     webSocketRequest.onmessage = (streamEvent) => {
-      const parsedRecords = JSON.parse(streamEvent.data);
-      if (parsedRecords.type === 'PAGE_END') {
-        webSocketRequest.close();
-        console.log('Websocket Endpoint CLOSED');
-      }
-      console.log('Parsed Records', parsedRecords);
-      return parsedRecords;
+      of(streamEvent).pipe(
+        map(event => JSON.parse(event.data)),
+        filter(message => message.type === 'RECORD')
+      ).subscribe(message => messages.push(message.data.value));
     };
   };
+  return messages;
 });
 
+const data = wsRequest().then(results => console.log(results, 'R'));
+const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+  width = 900 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-wsRequest();
+const svg = d3.select("#my_dataviz")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+      .domain(d3.extent(data, function (d) { return d.merchantId; }))
+      .range([0, width]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function (d) { return +d.amount; })])
+      .range([height, 0]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function (d) {
+          console.log(d, 'inside');
+          return x(d.merchantId);
+        })
+        .y(function (d) {
+          return y(d.amount);
+        })
+      );
+
